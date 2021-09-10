af1683a55350714f29049d8671618d0663764309
baldmaan <soltyspozdro@gmail.com>
2021-09-10 22:50:41 +0200

    homepage: navbar, first section, footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { DescriptionComponent } from './components/description/description.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DescriptionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
