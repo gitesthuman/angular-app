996b09bc764b2f8c450c821605f19d8eb6c35a9d
gitesthuman <90265591+gitesthuman@users.noreply.github.com>
2021-09-14 17:20:54 +0200

    created contact page

---
+++ app/components/description/contact/contact.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+//import '../../../../assets/smtp.js';
+
+declare let Email: any;
+
+@Component({
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent implements OnInit {
+
+  name!: string;
+  email!: string;
+  message!: string;
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  submitForm() {
+    alert(this.name + ', ' + this.email + ', ' + this.message);
+    window.location.reload();
+  }
+
+}
