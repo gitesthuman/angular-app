09cd26dc9f35b91d4b89e5fe7d474dc8c3e9efab
baldmaan <soltyspozdro@gmail.com>
2021-09-15 14:39:49 +0200

    changes in contact.component.ts by zmudson

--- src/app/components/description/contact/contact.component.ts
+++ src/app/components/description/contact/contact.component.ts
@@ -5,21 +5,22 @@ declare let Email: any;
 
 @Component({
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
-
   name!: string;
   email!: string;
   message!: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
   submitForm() {
-    alert(this.name + ', ' + this.email + ', ' + this.message);
-    window.location.reload();
+    // alert(this.name + ', ' + this.email + ', ' + this.message);
+    // window.location.reload();
+    this.name = '';
+    this.email = '';
+    this.message = '';
   }
-
 }
